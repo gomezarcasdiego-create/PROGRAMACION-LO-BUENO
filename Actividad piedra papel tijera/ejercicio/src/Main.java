841d0aa491d661053331e00ee2b972002eaff4e2
COMPU-S022\daw1 <elidevaco@gmail.com>
2025-11-05 09:05:00 +0100

    Inicio del juego

---
+++ Actividad piedra papel tijera/ejercicio/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        //Ejercicio piedra papel tijera
+        System.out.println("Ejercicio juego piedra papel tijera: ");
+        System.out.println("\n juego de manos para dos o más personas " +
+                "donde cada jugador elige entre tres gestos: piedra " +
+                "(puño cerrado), papel (mano extendida)" +
+                " o tijera (dedos índice y medio separados)");
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
