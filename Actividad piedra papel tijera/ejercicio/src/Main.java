b8367e0db75f3b4417c2e719fcba2e9da03b1b26
COMPU-S022\daw1 <elidevaco@gmail.com>
2025-11-05 09:50:16 +0100

    no message

--- Actividad piedra papel tijera/ejercicio/src/Main.java
+++ Actividad piedra papel tijera/ejercicio/src/Main.java
@@ -9,7 +9,13 @@ public class Main {
                 "(puño cerrado), papel (mano extendida)" +
                 " o tijera (dedos índice y medio separados)");
         Scanner sc = new Scanner(System.in);
+        int jugador1 = 0 , jugador2 = 0 , partida = 0;
 
-        
+        do {
+            String [] opciones = {"p, l, t, s"};
+            System.out.println("escribe tu elección: ");
+            String eleccion = sc.nextLine();
+            System.out.println("Su eleccion es: " + eleccion);
+        } while(true);
     }
 }
