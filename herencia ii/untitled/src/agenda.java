8c70c2d04e7e227d4af1e039ebb0bc90c88063f5
COMPU-S022\daw1 <dgomeza01@educantabria.es>
2025-12-17 09:48:43 +0100

    herencia ii

---
+++ herencia ii/untitled/src/agenda.java
@@ -0,0 +1,16 @@
+public class agenda {
+    private contacto[] contactos;
+
+    public agenda() {
+        this.contactos = contactos;
+    }
+
+    public contacto[] getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(contacto[] contactos) {
+        this.contactos = contactos;
+    }
+
+}
