af0295a999ca48e14cb995ea400e9a79d703e96c
COMPU-S022\daw1 <dgomeza01@educantabria.es>
2026-01-14 09:52:06 +0100

    final de la actividad

--- UD4 P2/colecciones/src/Contacto.java
+++ UD4 P2/colecciones/src/Contacto.java
@@ -1,8 +1,14 @@
+import java.util.regex.Pattern;
+
 public class Contacto {
 
     private String nombre;
     private String telefono;
     private String Correo;
+
+    Pattern nombreD = Pattern.compile("(A-Z¨{0,}[a-z])");
+    Pattern telefonoD = Pattern.compile("(6/7/9{8}[0-9])");
+    Pattern CorreoD = Pattern.compile("[^a-z0-9_.-]+@[^a-z]+\\.[a-z]{2,4}€");
 
     public Contacto(String nombre, String telefono, String correo) {
         this.nombre = nombre;
@@ -28,5 +34,14 @@ public class Contacto {
 
     public void setCorreo(String correo) {
         Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", Correo='" + Correo + '\'' +
+                '}';
     }
 }
--- UD4 P2/colecciones/src/Main.java
+++ UD4 P2/colecciones/src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    Agenda agenda = new Agenda();
+    int opcion = 0;
+
+    do{
+        System.out.println("\n----- MENÚ AGENDA -----");
+        System.out.println("1. Añadir Contacto");
+        System.out.println("2. Buscar Contactos");
+        System.out.println("3. Eliminar Contacto");
+        System.out.println("4. Visusalizar agenda");
+        System.out.println("5. Número de contactos");
+        System.out.println("6. Salir");
+
+        opcion = sc.nextInt();
+        sc.nextLine();
+
+        switch(opcion){
+            case 1:
+                agenda.anadeContacto();
+                break;
+            case 2:
+                agenda.buscaContacto();
+                break;
+            case 3:
+                agenda.eliminaContacto();
+                break;
+            case 4:
+                agenda.visualizaAgenda();
+                break;
+            case 5:
+                System.out.println("Número de contactos: " + agenda.numeroContactos());
+                break;
+            case 6:
+                System.out.println("Saliendo del programa");
+                break;
+
+            default:
+                System.out.println("opcion invalida");
+        }
+    }while (opcion != 6);
+
+    sc.close();
 
     }
 }
