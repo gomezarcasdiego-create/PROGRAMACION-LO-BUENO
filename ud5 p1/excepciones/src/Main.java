d9818620c4ebc8c50693f82a2e925de37f0a29b0
COMPU-S022\daw1 <dgomeza01@educantabria.es>
2026-01-23 09:54:35 +0100

    inicio del ejercicio 1 ud5

---
+++ ud5 p1/excepciones/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void Ejercicio1() {
+
+    }
+
+    public static void Ejercicio2() {
+
+    }
+
+    public static void Ejercicio3() {
+
+    }
+
+    public static void Ejercicio41() {
+
+    }
+
+    public static void Ejercicio5() {
+
+    }
+
+    public static void Ejercicio6() {
+
+    }
+}
