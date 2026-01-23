ed60ccba8e830d4d8ae7c3c53d60fd56da512918
COMPU-S022\daw1 <dgomeza01@educantabria.es>
2026-01-23 09:30:29 +0100

    terminada p2

--- UD4 P2/colecciones Taller/src/Main.java
+++ UD4 P2/colecciones Taller/src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public static <no> void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Taller taller = new Taller();
-        int opcion = 0;
+        String opcion ;
 
         do {
             System.out.println("\n----MENU TALLER----");
@@ -13,24 +13,25 @@ public class Main {
             System.out.println("2. Eliminar Coche");
             System.out.println("3. Salir");
 
-            opcion = sc.nextInt();
+            opcion = sc.nextLine();
+
             switch (opcion) {
-                case 1:
+                case "1":
                     taller.anadeCoche();
                     break;
 
-                case 2:
+                case "2":
                     taller.eliminaCoche();
                     break;
 
-                case 3:
+                case "3":
                     System.out.println("Saliendo del programa");
                     break;
 
                 default:
                     System.out.println("Opcion no valida");
             }
-        } while (opcion != 3);
+        }while(!opcion.equals("3"));
 
         System.out.println("\n---- ESTADO FINAL DEL TALLER ----");
         taller.visualizaTaller();
